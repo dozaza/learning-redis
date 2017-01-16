cc3d1f786196cd05d863d0b0c2512fec3494ab0b
Dozaza <baoan.feng@gmail.com>
2017-01-16 23:36:18 +0800

    update dependency

--- build.sbt
+++ build.sbt
@@ -4,5 +4,6 @@ version := "0.0.x"
 
 libraryDependencies ++= {
   Seq(
+    "com.github.etaty" %% "rediscala" % "1.7.0"
   )
 }
---
+++ src/main/scala/main.scala
@@ -0,0 +1,9 @@
+import redis.RedisClient
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+object main {
+
+}
