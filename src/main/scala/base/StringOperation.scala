022f5d133a082735b0c9a82f5b78e2aad5b9eb8b
Dozaza <baoan.feng@gmail.com>
2017-01-16 23:55:28 +0800

    add connect closure

---
+++ src/main/scala/base/StringOperation.scala
@@ -0,0 +1,6 @@
+
+package base
+
+object StringOperation {
+
+}
---
+++ src/main/scala/redis/dsl.scala
@@ -0,0 +1,17 @@
+package redis
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+package object dsl {
+
+  def connect[T](op: RedisClient => Future[T]): T = {
+    implicit val akkaSystem = akka.actor.ActorSystem()
+    val redis = RedisClient()
+    val future = op(redis)
+    val result = Await.result(future, 1 hours)
+    akkaSystem.shutdown()
+    result
+  }
+
+}
